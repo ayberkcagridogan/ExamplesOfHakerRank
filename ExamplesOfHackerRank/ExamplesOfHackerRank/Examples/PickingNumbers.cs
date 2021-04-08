98fb61ed7b0eb2750b9c52ec73e8cd0b5b1bf2d4
ponciksevgilim <ayberkcagridogan@gmail.com>
2021-04-08 10:18:54 +0200

    Add PickingNumbers

---
+++ ExamplesOfHackerRank/ExamplesOfHackerRank/Examples/PickingNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamplesOfHackerRank.Examples
+{
+    public class PickingNumbers
+    {
+
+        public int FindMaxSubArray(List<int> a)
+        {
+            int result = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                int count = a.FindAll(x => x == a[i]).Count + a.FindAll(x => x == a[i] + 1).Count;
+
+                if (count > result)
+                {
+                    result = count;
+                }
+
+            }
+            return result;
+        }
+    }
+}
