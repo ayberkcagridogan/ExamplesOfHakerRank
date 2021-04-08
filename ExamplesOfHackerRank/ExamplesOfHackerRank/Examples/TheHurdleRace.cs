3f3daf59dedc64abaafeb3ae17a308814ca9f315
ponciksevgilim <ayberkcagridogan@gmail.com>
2021-04-08 10:07:20 +0200

    Add The Hurdle Race example

---
+++ ExamplesOfHackerRank/ExamplesOfHackerRank/Examples/TheHurdleRace.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ExamplesOfHackerRank.BusinessHelp;
+
+namespace ExamplesOfHackerRank.Examples
+{
+    public class TheHurdleRace
+    {
+
+        public int FindMaxSupport(int k, int[] height)
+        {
+
+            int[] sortHeight = new HeapSort().Sort(height);
+
+            if (sortHeight[sortHeight.Length - 1] - k <= 0 )
+            {
+                return 0;
+            }
+
+            if(sortHeight[sortHeight.Length - 1] - k > 0)
+            {
+                return sortHeight[sortHeight.Length - 1] - k;
+            }
+
+            return -1;
+
+        }
+
+
+    }
+}
---
+++ ExamplesOfHackerRank/ExamplesOfHackerRankUnitTest2/ExamplesTest/TheHurdleRaceTestClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using ExamplesOfHackerRank.Examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExamplesOfHackerRankUnitTest.ExamplesTest
+{
+    [TestClass]
+    public class TheHurdleRaceTestClass
+    {
+        [TestMethod]
+        public void PickingNumberTest_Case1()
+        {
+
+            var k = 4;
+            var height = new int[] { 1 ,6 , 3 ,5 , 2};
+
+
+            var result = new TheHurdleRace().FindMaxSupport(k, height);
+
+            var expect = 2;
+
+            Assert.AreEqual(expect, result);
+
+        }
+
+        [TestMethod]
+        public void PickingNumberTest_Case2()
+        {
+
+            var k = 7;
+            var height = new int[] { 2 , 5, 4, 5, 2};
+
+
+            var result = new TheHurdleRace().FindMaxSupport(k, height);
+
+            var expect = 0;
+
+            Assert.AreEqual(expect, result);
+
+        }
+    }
+}
