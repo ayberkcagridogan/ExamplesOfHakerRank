bc43ac4fd73642a6466dfbae062f65bfe3fff86c
ponciksevgilim <ayberkcagridogan@gmail.com>
2021-04-08 10:20:06 +0200

    Fix Unit Test Name

--- ExamplesOfHackerRank/ExamplesOfHackerRankUnitTest/ExamplesTest/PickingNumbersTestClass.cs
+++ ExamplesOfHackerRank/ExamplesOfHackerRankUnitTest/ExamplesTest/PickingNumbersTestClass.cs
@@ -1,21 +1,38 @@
 using System;
 using System.Collections.Generic;
-using Xunit;
 using ExamplesOfHackerRank.Examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ExamplesOfHackerRankUnitTest.ExamplesTest
 {
+    [TestClass]
     public class PickingNumbersTestClass
     {
-        [Fact]
+        [TestMethod]
         public void PickingNumberTest_Case1()
         {
-            var a = new List<int>() { 1, 2, 4, 6, 6, 7 };
+            var a = new List<int>() { 4,6,5,3,3,1};
+            int expect = 3;
 
             var picknumbers = new PickingNumbers();
 
-            picknumbers.pickingNumbers(a);
+           int result = picknumbers.FindMaxSubArray(a);
 
+            Assert.AreEqual(expect, result);
+
+        }
+
+        [TestMethod]
+        public void PickingNumberTest_Case2()
+        {
+            var a = new List<int>() { 1, 2,2,3,1,2};
+            int expect = 5;
+
+            var picknumbers = new PickingNumbers();
+
+            int result = picknumbers.FindMaxSubArray(a);
+
+            Assert.AreEqual(expect, result);
 
         }
     }
