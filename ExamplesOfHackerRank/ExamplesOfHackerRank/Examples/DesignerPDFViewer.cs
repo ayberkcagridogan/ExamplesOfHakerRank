cede9365a10f2750ef7d25597e6c8844dc495837
ponciksevgilim <ayberkcagridogan@gmail.com>
2021-04-09 11:26:23 +0200

    Add Designer PDF Viewer

---
+++ ExamplesOfHackerRank/ExamplesOfHackerRank/Examples/DesignerPDFViewer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamplesOfHackerRank.Examples
+{
+    public class DesignerPDFViewer
+    {
+        public int FindHighlightedText(int[] heghlighted , string word)
+        {
+            List<string> letterIndex = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w" ,"x", "y", "z" };
+
+            int maxheightedLetter = 0;
+            
+            for (int i = 0; i < word.Length; i++)
+            {
+                int letterHeighted = heghlighted[letterIndex.LastIndexOf(word.Substring(i, 1))];
+                if (letterHeighted > maxheightedLetter)
+                {
+                    maxheightedLetter = letterHeighted;
+                }
+            }
+
+            return word.Length * maxheightedLetter;
+        }
+    }
+}
---
+++ ExamplesOfHackerRank/ExamplesOfHackerRankUnitTest/ExamplesTest/DesignerPDFViewerTestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ExamplesOfHackerRank.Examples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExamplesOfHackerRankUnitTest.ExamplesTest
+{
+    [TestClass]
+    public class DesignerPDFViewerTestClass
+    {
+        [TestMethod]
+        public void DesignerPDFViewerTest_Case1()
+        {
+            int[] heghlighted = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
+            string word = "abc";
+
+            var actual = new DesignerPDFViewer().FindHighlightedText(heghlighted, word);
+
+            var expected = 9;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DesignerPDFViewerTest_Case2()
+        {
+            int[] heghlighted = { 1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7 };
+            string word = "zaba";
+
+            var actual = new DesignerPDFViewer().FindHighlightedText(heghlighted, word);
+
+            var expected = 28;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
