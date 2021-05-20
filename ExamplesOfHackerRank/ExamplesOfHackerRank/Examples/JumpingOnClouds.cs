31d4df12327d98526ae94369317f8847c84fb041
ayberkcagridogan <ayberkcagridogan@gmail.com>
2021-05-20 14:18:05 +0200

    Add JumpıngOnClouds

---
+++ ExamplesOfHackerRank/ExamplesOfHackerRank/Examples/JumpingOnClouds.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamplesOfHackerRank.Examples
+{
+    public class JumpingOnClouds {
+       
+        public int CalculateEnergy(int[] c, int k)
+        {
+            int energy = 100;
+    
+            for (int i = 0; i < c.Length; i += k)
+            {
+
+            }
+
+
+            return energy;
+
+        }
+    } 
+}
